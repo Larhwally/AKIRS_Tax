e00b9f18052ebd7fe35ee08691bef061e07847ee
Larhwally <52761470+Larhwally@users.noreply.github.com>
2019-07-16 20:11:24 +0100

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tax_AK.API.WebApiApplication" Language="C#" %>
---
+++ PostingResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tax_AK.Data
+{
+    public class PostingResponse
+    {
+        public int respcode { get; set; }
+        public string responseMsg { get; set; }
+    }
+}
